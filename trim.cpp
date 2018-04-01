6c8765c3f5f6123ab77c4be9510bae5d6e048826
shwilliams783 <shwilliams783@gmail.com>
2018-04-01 15:44:39 -0500

    Linear Trim Functionality
    Added logic to remove linear regions from spherical clusters

--- trim.cpp
+++ trim.cpp
@@ -18,6 +18,8 @@ int main(int argc, char ** argv)
 	== NULL))
     perror("File could not be opened.\n");
 	
+	// Skip to graph declaration
+	
 	while (1) { // throw away everything until get to "graph" declaration
 	if(feof(input)) 
 		perror("no graph to read in input file");
@@ -28,26 +30,31 @@ int main(int argc, char ** argv)
 		break; // stop when get to "graph" declaration
 	}
 	
+	// Read and store each vertex as a node in nodes; store original ID as well
+	
 	while (1) { // throw away everything until get to "edge" declaration
-	if(feof(input))
-		perror("no edges to read in input file");
+		if(feof(input))
+			perror("no edges to read in input file");
 
-	fscanf(input, "%s", string); // read in string from file
+		fscanf(input, "%s", string); // read in string from file
 
-	if(strncmp(string, "id", 2) == 0) { 
-		fscanf(input, "%s", string); // read in id value
-		newNode->originalID = atoi(string); //assign id value to new node
-		numNodes++; // one more node found
-		//std::cout<<"originalId = "<<newNode->originalID<<" numNodes = "<<numNodes<<std::endl;
-		nodes.push_back(*newNode); // add newNode to nodes list
-		nodeIt++; // increment node list iterator
-		//std::cout<<"nodeIt.originalID = "<<(*nodeIt).originalID<<" nodes.size = "<<nodes.size()<<std::endl;
+		if(strncmp(string, "id", 2) == 0) { 
+			fscanf(input, "%s", string); // read in id value
+			newNode->originalID = atoi(string); //assign id value to new node
+			numNodes++; // one more node found
+			//std::cout<<"originalId = "<<newNode->originalID<<" numNodes = "<<numNodes<<std::endl;
+			nodes.push_back(*newNode); // add newNode to nodes list
+			nodeIt++; // increment node list iterator
+			//std::cout<<"nodeIt.originalID = "<<(*nodeIt).originalID<<" nodes.size = "<<nodes.size()<<std::endl;
+		}
+			
+
+		if(strncmp(string, "edge", 4) == 0) 
+			break; // stop when read in first "edge"
 	}
-
-	if(strncmp(string, "edge", 4) == 0) 
-		break; // stop when read in first "edge"
-	}
-
+	
+	// Read edges and store neighboring vertices in edges list
+	
 	while (!feof(input)) { // read edges until reach end of file
 		fscanf(input, "%s", string); // read in string from file
 
@@ -67,6 +74,21 @@ int main(int argc, char ** argv)
 
 		fscanf(input, "%s", string); // read in target node from file
 		int target = atoi(string);
+		
+		std::list<node_t>::iterator edgeIt;
+		for(edgeIt = nodes.begin(); edgeIt != nodes.end(); edgeIt++)
+		{
+			if((*edgeIt).originalID == source)
+			{
+				(*edgeIt).edges.push_back(target);
+				std::cout << target << " added to source edge list." << std::endl;
+			}
+			if((*edgeIt).originalID == target)
+			{
+				(*edgeIt).edges.push_back(source);
+				std::cout << source << " added to target edge list." << std::endl;
+			}				
+		}
 
 		fscanf(input, "%s", string); // read in string from file
 
@@ -96,22 +118,112 @@ int main(int argc, char ** argv)
 
 		if(!strncmp(string, "edge", 4) == 0) // check for correct character
 			perror("No 'edge' declaration");
-			
-		std::list<node_t>::iterator edgeIt;
-		for(edgeIt = nodes.begin(); edgeIt != nodes.end(); edgeIt++)
-		{
-			if((*edgeIt).originalID == source)
-			{
-				(*edgeIt).edges.push_back(target);
-				std::cout << target << " added to source edge list." << std::endl;
-			}
-			if((*edgeIt).originalID == target)
-			{
-				(*edgeIt).edges.push_back(source);
-				std::cout << source << " added to target edge list." << std::endl;
-			}				
-		}
-	}  
+		
+	} 
 
+	// Node and edge display
+	for(nodeIt = nodes.begin(); nodeIt != nodes.end(); nodeIt++)
+	{
+		std::cout << "nodeIt.originalID = " << (*nodeIt).originalID << ", edges: ";
+		std::list<int>::iterator edgeIt;
+		for(edgeIt = (*nodeIt).edges.begin(); edgeIt != (*nodeIt).edges.end(); edgeIt++)
+		{
+			std::cout << (*edgeIt) << ", ";
+		}
+		std::cout << std::endl;
+	}
+
+	// Search for nodes containing only one edge
+	int prevNode;
+	int neighbor;
+	
+	std::list<node_t>::iterator terminalIt;
+	std::list<node_t>::iterator targetIt;
+	for(terminalIt = nodes.begin(); terminalIt != nodes.end(); terminalIt++)
+	{
+		if((*terminalIt).edges.size() == 1) // this is a terminal vertex
+		{
+			std::cout << "Terminal vertex found at ID = " << (*terminalIt).originalID << std::endl;
+			prevNode = (*terminalIt).originalID; // store the first ID of the terminal vertex
+			neighbor = (*terminalIt).edges.front(); // the first neighbor will be the ID of the only vertex in the edges list of the terminal vertex
+			
+			// the edge to be removed will be between neighbor and prevNode once the "linear cluster" reaches a proper cluster or another terminal vertex
+			targetIt = nodes.begin(); // initialize the target iterator
+			advance(targetIt, neighbor-1); // advance target iterator to the neighbor node in the nodes list
+			
+			std::cout << "BEFORE WHILE targetIt.originalID " << (*targetIt).originalID << " and edges.size() = " << (*targetIt).edges.size() << std::endl;
+			
+			// check the number of edges in the neighboring node until a node
+			// with exactly 1 edge, or 3+ edges is found:
+			// case 1: do nothing; the cluster is completely linear.
+			// case 3+: remove the edge beween the 3+ edges node and the prevNode 
+			while((*targetIt).edges.size() == 2)
+			{
+				if ((*targetIt).edges.front() != prevNode)
+				{
+					prevNode = neighbor;
+					neighbor = (*targetIt).edges.front();
+				}
+				else
+				{
+					prevNode = neighbor;
+					neighbor = (*targetIt).edges.back();
+				}
+				targetIt = nodes.begin();
+				advance(targetIt, neighbor-1);
+				std::cout << "targetIt.originalID " << (*targetIt).originalID << " and edges.size() = " << (*targetIt).edges.size() << std::endl;
+			}
+			// at this point, there is either a cluster or a linear terminal
+			//at the neighbor vertex
+			
+			if((*targetIt).edges.size() >= 3) // neighbor is a cluster
+			{
+				std::list<node_t>::iterator edgeIt;
+				for(edgeIt = nodes.begin(); edgeIt != nodes.end(); edgeIt++)
+				{
+					if((*edgeIt).originalID == neighbor)
+					{
+						std::list<int>::iterator cutIt;
+						for(cutIt = (*edgeIt).edges.begin(); cutIt != (*edgeIt).edges.end(); cutIt++)
+						{
+							if((*cutIt) == prevNode)
+							{
+								cutIt = (*edgeIt).edges.erase(cutIt);
+							}
+						}
+						std::cout << "cut edge from " << neighbor << " to " << prevNode << std::endl;
+					}	
+					else if((*edgeIt).originalID == prevNode)
+					{
+						std::list<int>::iterator cutIt;
+						for(cutIt = (*edgeIt).edges.begin(); cutIt != (*edgeIt).edges.end(); cutIt++)
+						{
+							if((*cutIt) == neighbor)
+							{
+								cutIt = (*edgeIt).edges.erase(cutIt);
+							}
+						}
+						std::cout << "cut edge from " << prevNode << " to " << neighbor << std::endl;
+					}	
+				}
+			}
+			std::cout << "end linear trim" << std::endl;
+			
+			// Node and edge display
+			for(nodeIt = nodes.begin(); nodeIt != nodes.end(); nodeIt++)
+			{
+				std::cout << "nodeIt.originalID = " << (*nodeIt).originalID << ", edges: ";
+				std::list<int>::iterator edgeIt;
+				for(edgeIt = (*nodeIt).edges.begin(); edgeIt != (*nodeIt).edges.end(); edgeIt++)
+				{
+					std::cout << (*edgeIt) << ", ";
+				}
+				std::cout << std::endl;
+			}
+		}
+		std::cout << "end terminal search iteration" << std::endl;
+	}
+	
+	
 	return 1;	
 }
